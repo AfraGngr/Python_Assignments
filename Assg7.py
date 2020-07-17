6e5c02f84a0793fbf0af57582339ccbe22b064c9
afrag <gungorafra93@gmail.com>
2020-07-17 20:53:19 +0300

    FizzBuzz

---
+++ Assg7.py
@@ -0,0 +1,12 @@
+num = list(range(101))
+
+for i in num:
+    
+    if i % 3 == 0 and i % 5 != 0:
+        print("Fizz")
+    elif  i % 5 == 0 and i % 3 != 0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0 :
+        print("FizzBuzz")
+    else:
+        print(i)
